2e1bbc1ae1c0d752497e79c32024988e2711a186
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-29 09:05:50 +0530

    view commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsEntryComponent } from './news-entry/news-entry.component';
+import { NewsViewComponent } from './news-view/news-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsEntryComponent
+    NewsEntryComponent,
+    NewsViewComponent
   ],
   imports: [
     BrowserModule,
